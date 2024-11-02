86323af5a598fbdaca4a58bb77869277dde34509
Javad Khorashadizade <43370342+khorashadi666@users.noreply.github.com>
2024-11-02 17:28:25 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,4 +35,4 @@ do
 done
 
 # Do deployment
-sh -c ""rsync $ARGS $EXCLUDE -e 'ssh -i $SSH_PATH/deploy_key $SSH_CMD_ARGS -p $PORT_NUMBER'"
+sh -c "rsync $ARGS $EXCLUDE -e 'ssh -i $SSH_PATH/deploy_key $SSH_CMD_ARGS -p $PORT_NUMBER'"
