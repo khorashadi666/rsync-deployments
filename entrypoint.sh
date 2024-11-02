5eb97d9e65c73a065680db605c8e76ba21cd4888
Javad Khorashadizade <43370342+khorashadi666@users.noreply.github.com>
2024-11-02 20:14:29 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,4 +35,4 @@ do
 done
 
 # Do deployment
-sh -c "rsync $ARGS $EXCLUDE -e 'ssh -i $SSH_PATH/deploy_key $SSH_CMD_ARGS -p $PORT_NUMBER' --progress $CHOWN ${GITHUB_WORKSPACE}$SOURCE ${REMOTE_USER}@${REMOTE_HOST}:$TARGET"
+sh -c "rsync $ARGS $EXCLUDE -e 'ssh -p 22' --progress $CHOWN ${GITHUB_WORKSPACE}$SOURCE ${REMOTE_USER}@${REMOTE_HOST}:$TARGET"
