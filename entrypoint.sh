7b59e955f4460c28978fbe90203bfddbbb6abbc5
Javad Khorashadizade <43370342+khorashadi666@users.noreply.github.com>
2024-11-02 18:11:46 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,4 +35,4 @@ do
 done
 
 # Do deployment
-sh -c "rsync $ARGS $EXCLUDE -e 'ssh -i $SSH_PATH/deploy_key $SSH_CMD_ARGS -p 22' $CHOWN ${GITHUB_WORKSPACE}$SOURCE root@212.33.205.190:/lingemy/project/school/prod/code/wp-test"
+sh -c "rsync $ARGS $EXCLUDE -e 'ssh -i $SSH_PATH/deploy_key -p 22' $CHOWN ${GITHUB_WORKSPACE}$SOURCE root@212.33.205.190:/lingemy/project/school/prod/code/wp-test"
